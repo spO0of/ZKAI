976bda282e7a5850a7d2804601662edbf1ccb7c8
carambole25 <carambole258@gmail.com>
2025-12-06 13:06:29 +0100

    POC : Auth process : bloqué sur la partie vérification du JWT

--- POC/api/main.py
+++ POC/api/main.py
@@ -25,8 +25,8 @@ class CryptoChallengeClear(BaseModel):
     crypto_challenge_clear: str
 
 class UserJwt(BaseModel):
-    name_encrypted: str
-    email_encrypted: str
+    name: str
+    email: str
 
 
 app = FastAPI()
@@ -64,6 +64,40 @@ async def input_request(id: Id):
 @app.post("/login_resolve_crypto_challenge_and_send_jwt")
 async def input_request(id: Id, crypto_challenge_clear: CryptoChallengeClear, user_jwt : UserJwt):
     if id_challenges_cache_memory[id.id].decode() == crypto_challenge_clear.crypto_challenge_clear:
-        print("ok")
+
+        user_data = db.get(doc_id=id.id)
+        public_key_str = user_data['pub']
+        public_key_data = public_key_str.encode('utf-8')
+        public_key = serialization.load_pem_public_key(public_key_data)
+        pub_nums = public_key.public_numbers()
+        e = pub_nums.e
+        n = pub_nums.n
+
+        decrypted_jwt = {}
+
+        for key, value in user_jwt:
+            value = int.from_bytes(base64.b64decode(value), "big")
+            m = pow(value, e, n)
+            pt = m.to_bytes((m.bit_length()+7)//8, "big")
+            decrypted_jwt[key] = base64.b64encode(pt).decode()
+
+        encrypted_jwt = {}
+
+        for key, value in decrypted_jwt.items():
+            encrypted_data = public_key.encrypt(
+                value.encode("utf-8"),
+                padding.OAEP(
+                    mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                    algorithm=hashes.SHA256(),
+                    label=None
+                )
+            )
+            encrypted_jwt[key] = base64.b64encode(encrypted_data)
+        print(encrypted_jwt)
+        """
+        là le problème c'est que ça donne pas le même résultat parce que le chiffrement est pas déterministe pour des raisons de sécurité.
+        je peu pas non plus faire un hahs des données reçu lors du register parce que les données sont jamais reçu en clair avant maintenant...
+        là je suis bloqué
+        """
     else:
         print("nok")
